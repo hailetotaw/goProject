6949d7c21ed590d1e2dbd614aa4216e29f30143c
unknown <haile.kassa.mamo@gmail.com>
2019-09-08 14:53:07 -0400

    translation working successfully

--- main.go
+++ main.go
@@ -10,50 +10,30 @@ import (
 	"github.com/nlopes/slack"
 )
 
+// environment variables used are placed under a truct
 type envConfig struct {
-	// Port is server port to be listened.
-	Port string `envconfig:"PORT" default:"5000"`
-
-	// BotToken is bot user token to access to slack API.
-	BotToken string `envconfig:"BOT_TOKEN" required:"true"`
-
-	// VerificationToken is used to validate interactive messages from slack.
+	Port              string `envconfig:"PORT" default:"5000"`
+	BotToken          string `envconfig:"BOT_TOKEN" required:"true"`
 	VerificationToken string `envconfig:"VERIFICATION_TOKEN" required:"true"`
-
-	// BotID is bot user ID.
-	BotID string `envconfig:"BOT_ID" required:"true"`
-
-	// ChannelID is slack channel ID where bot is working.
-	// Bot responses to the mention in this channel.
-	ChannelID string `envconfig:"CHANNEL_ID" required:"false"`
+	BotID             string `envconfig:"BOT_ID" required:"true"`
+	ChannelID         string `envconfig:"CHANNEL_ID" required:"false"`
 }
 
-// SlackListener data type
-type SlackListener struct {
-	api       *slack.Client
-	botID     string
-	channelID string
-}
-
+// added the struct below to verify this app from slack
 type slackResponse struct {
 	Token     string
 	Challenge string
 	Type      string
 }
 
-const (
-	// action is used for slack attament action.
-	actionSelect = "select"
-	actionStart  = "start"
-	actionCancel = "cancel"
-)
-
 func main() {
+	//used github.com/kelseyhightower/envconfig library to get all the list of env variables
 	var env envConfig
 	if err := envconfig.Process("", &env); err != nil {
 		fmt.Printf("[ERROR] Failed to process env var: %s", err)
 	}
-
+	// github.com/nlopes/slack library supports most if not all of the api.slack.com REST calls,
+	// as well as the Real-Time Messaging protocol over websocket, in a fully managed way.
 	api := slack.New(env.BotToken)
 
 	slackListener := &SlackListener{
@@ -65,40 +45,12 @@ func main() {
 	go slackListener.ListenAndResponse()
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/echo", authenticateSlack)
 	mux.HandleFunc("/learnAmharic", learnAmharic)
 
 	http.ListenAndServe(":5000", mux)
 }
 
-// "ListenAndResponse method respond to event changes on slack channel"
-func (s *SlackListener) ListenAndResponse() {
-	rtm := s.api.NewRTM()
-
-	// Start listening slack events
-	go rtm.ManageConnection()
-
-	// Handle slack events
-	for msg := range rtm.IncomingEvents {
-		switch ev := msg.Data.(type) {
-		case *slack.MessageEvent:
-			if err := s.handleMessageEvent(ev); err != nil {
-				fmt.Printf("[ERROR] Failed to handle message: %s", err)
-			}
-		}
-	}
-}
-
-func (s *SlackListener) handleMessageEvent(ev *slack.MessageEvent) error {
-	// value is passed to message handler when request is approved.
-	if _, _, err := s.api.PostMessage(ev.Channel, slack.MsgOptionText("Some text", true)); err != nil {
-		return fmt.Errorf("failed to post message: %s", err)
-	}
-
-	return nil
-}
-
-func authenticateSlack(writer http.ResponseWriter, request *http.Request) {
+func learnAmharic(writer http.ResponseWriter, request *http.Request) {
 
 	slackMessage := slackResponse{}
 
@@ -114,15 +66,4 @@ func authenticateSlack(writer http.ResponseWriter, request *http.Request) {
 	writer.WriteHeader(http.StatusOK)
 	writer.Header().Set("Content-Type", "text/plain")
 	writer.Write([]byte(slackMessage.Challenge))
-}
-
-func learnAmharic(w http.ResponseWriter, r *http.Request) {
-	buf, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Println("Error while getting request to the bot via /learningAmharic")
-	}
-	fmt.Println(string(buf))
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("success"))
 }
---
+++ slackListner.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/nlopes/slack"
+)
+
+// SlackListener data type
+type SlackListener struct {
+	api       *slack.Client
+	botID     string
+	channelID string
+}
+
+const (
+	// action is used for slack attament action.
+	actionSelect = "select"
+	actionStart  = "start"
+	actionCancel = "cancel"
+)
+
+// "ListenAndResponse method respond to event changes on slack channel"
+func (s *SlackListener) ListenAndResponse() {
+	rtm := s.api.NewRTM()
+
+	// Start listening slack events
+	go rtm.ManageConnection()
+
+	// Handle slack events
+	for msg := range rtm.IncomingEvents {
+		switch ev := msg.Data.(type) {
+		case *slack.MessageEvent:
+			if err := s.handleMessageEvent(ev); err != nil {
+				fmt.Printf("[ERROR] Failed to handle message: %s", err)
+			}
+		}
+	}
+}
+
+func (s *SlackListener) handleMessageEvent(ev *slack.MessageEvent) error {
+	fmt.Println(ev.Msg.Text, s.botID)
+	// make sure the BOT ID is mentioned
+	if !strings.HasPrefix(ev.Msg.Text, fmt.Sprintf("<@%s> ", s.botID)) {
+		fmt.Println(ev.Msg.Text)
+	}
+
+	// Parse message
+	m := strings.Split(strings.TrimSpace(ev.Msg.Text), " ")[1:]
+	if len(m) == 0 || m[0] != "translate" {
+		return fmt.Errorf("invalid message")
+	}
+
+	// get the actual work to translate
+	text := m[1:]
+	if len(text) == 0 {
+		return fmt.Errorf("No word or text to translate")
+	}
+
+	// value is passed to message handler when request is approved.
+	if _, _, err := s.api.PostMessage(ev.Channel, slack.MsgOptionText(translateToAmharic(text), true)); err != nil {
+		return fmt.Errorf("failed to post message: %s", err)
+	}
+
+	return nil
+}
